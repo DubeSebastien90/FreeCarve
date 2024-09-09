042f27a736bdfda444522f7c8887b5cba6334d31
Sébastien Dubé <sebastien.dube.10@ulaval.ca>
2024-09-09 08:38:32 -0400

    documentation for mesh and cube

--- src/main/java/Screen/Cube.java
+++ src/main/java/Screen/Cube.java
@@ -4,13 +4,27 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Creates a mesh in the shape of a cube.
+ * @author Sébastien Dubé
+ * @version 1.0
+ * @since 2024-09-08
+ */
 public class Cube extends Mesh {
 
+    /**
+     * Constructor for a Cube object
+     * @param position - the position of the cube in the scene
+     * @param color - the color of the cube
+     */
     public Cube(Vertex position, Color color) {
         super(position, color);
         setTrianglesList();
     }
 
+    /**
+     * Creates the triangles of the cube
+     */
     @Override
     public void setTrianglesList() {
         //create triangles
@@ -34,8 +48,22 @@ public class Cube extends Mesh {
         this.calculateCenter();
     }
 
+    /**
+     * Calculates the center of the mesh with the vertices
+     * @return the 3D coordinates of the center of the mesh
+     */
     @Override
-    public List<Triangle> getTrianglesList() {
-        return this.trianglesList;
+    public Vertex calculateCenter(){
+        double centerX = 0.0, centerY = 0.0, centerZ = 0.0;
+        for (Vertex v : verticesList) {
+            centerX += v.getX();
+            centerY += v.getY();
+            centerZ += v.getZ();
+        }
+        centerX = centerX / ((double) (verticesList.size()));
+        centerY = centerY / ((double) (verticesList.size()));
+        centerZ = centerZ / ((double) (verticesList.size()));
+        this.center.setVertex(new Vertex(centerX, centerY, centerZ));
+        return center;
     }
 }
--- src/main/java/Screen/Mesh.java
+++ src/main/java/Screen/Mesh.java
@@ -4,6 +4,12 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Mesh class allows complex solids made out of triangles to be rendered and modified in the scene.
+ * @author Sébastien Dubé
+ * @version 1.0
+ * @since 2024-09-08
+ */
 public abstract class Mesh {
     protected List<Triangle> trianglesList;
     protected List<Vertex> verticesList;
@@ -12,30 +18,34 @@ public abstract class Mesh {
     protected Color color;
     protected Vertex center;
 
+    /**
+     * Abstract constructor for a Mesh object
+     * @param position - the position of the mesh in the scene
+     * @param color - the color of the mesh
+     */
     public Mesh(Vertex position, Color color) {
         this.position = position;
         this.color = color;
         this.center = new Vertex(0,0,0);
     }
 
-    public Vertex calculateCenter() {
-        double centerX = 0.0, centerY = 0.0, centerZ = 0.0;
-        for (Vertex v : verticesList) {
-            centerX += v.getX();
-            centerY += v.getY();
-            centerZ += v.getZ();
-        }
-        centerX = centerX / ((double) (verticesList.size()));
-        centerY = centerY / ((double) (verticesList.size()));
-        centerZ = centerZ / ((double) (verticesList.size()));
-        this.center.setVertex(new Vertex(centerX, centerY, centerZ));
-        return center;
-    }
+    /**
+     * Calculates the center of the mesh depending on the shape
+     * @return the 3D coordinates of the center of the mesh
+     */
+    public abstract Vertex calculateCenter();
 
+    /**
+     * Returns the center point of the mesh
+     * @return the center of the mesh
+     */
     public Vertex getCenter() {
         return this.calculateCenter();
     }
 
+    /**
+     * Function that sets reference points of the vertices of the mesh in verticesList
+     */
     protected void setVerticesList(){
         List<Vertex> newVerticesList = new ArrayList<>();
         newVerticesList.add(this.trianglesList.get(0).getVertex1());
@@ -67,6 +77,9 @@ public abstract class Mesh {
         this.verticesList = newVerticesList;
     }
 
+    /**
+     * Function that sets the attribute edgesList at the creation of the mesh
+     */
     protected void findEdges(){
         List<List<Integer>> newEdgesList = new ArrayList<>();
         for(Triangle t : trianglesList){
@@ -75,14 +88,32 @@ public abstract class Mesh {
         this.edgesList = newEdgesList;
     }
 
+    /**
+     * Returns the mesh's vertices in a list
+     * @return the list of the mesh's Vertices
+     */
     public List<Vertex> getVerticesList(){
         return this.verticesList;
     }
 
+    /**
+     * Returns the list of the edges of the mesh in a tuple format. (1,2), (1,3), (2,4)... The integers represents the index of the Vertex in verticesList
+     * @return the edges of the mesh
+     */
     public List<List<Integer>> getEdgesList(){
         return this.edgesList;
     }
 
+    /**
+     * Creates the triangles of the mesh
+     */
     abstract void setTrianglesList();
-    abstract List<Triangle> getTrianglesList();
+
+    /**
+     * Returns the triangles of the mesh
+     * @return the triangles of the mesh in a list
+     */
+    List<Triangle> getTrianglesList(){
+        return this.trianglesList;
+    }
 }
