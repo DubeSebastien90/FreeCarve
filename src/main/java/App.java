732e551b100d4a66bbd6b41e431064b5664ad277
Louis-Etienne <louis-etienne.messier.1@ulaval.ca>
2024-09-21 12:10:39 -0400

    LaF

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,24 @@
-import UI.*;
+import Parser.ParsedSTL;
+import Parser.STLParser;
+import Screen.*;
+import Screen.Box;
+import Screen.Renderer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import com.formdev.flatlaf.FlatDarkLaf;
 
 public class App {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel( new FlatDarkLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
 
         MainWindow mainWindow = new MainWindow();
         mainWindow.start();
