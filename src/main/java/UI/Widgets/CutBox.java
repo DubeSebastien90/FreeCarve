2e6e805a05d8485e406569ecb329261f055f0894
Louis-Etienne <louis-etienne.messier.1@ulaval.ca>
2024-09-21 18:22:48 -0400

    dernier modif de ajd

--- src/main/java/UI/Widgets/CutBox.java
+++ src/main/java/UI/Widgets/CutBox.java
@@ -1,9 +1,19 @@
 package UI.Widgets;
 
+import Util.Cut;
+
+import javax.swing.*;
+
 /**
  * The {@code CutBox} class is a UI class that encapsulates a box containing all the informations
  * about a specific cut
+ *
+ * @author Louis-Etienne Messier
+ * @version 0.1
+ * @since 2024-09-21
  */
 public class CutBox {
+    private Cut cut;
+    private JPanel panel;
 
 }
