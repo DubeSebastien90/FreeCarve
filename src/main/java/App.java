10dafa0d1d843cd378352552f1c8a9ae901b3ec6
Kamran Charles Nayebi <kamran-charles.nayebi.1@ulaval.ca>
2024-10-12 18:18:37 -0400

    Fixed code that broke during rebase

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,6 @@
-import Parser.ParsedSTL;
-import Parser.STLParser;
-import Screen.*;
-import Screen.Box;
-import Screen.Renderer;
+import UI.*;
 
 import javax.swing.*;
-import java.awt.*;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import com.formdev.flatlaf.FlatDarkLaf;
 
 public class App {
@@ -22,31 +13,5 @@ public class App {
 
         MainWindow mainWindow = new MainWindow();
         mainWindow.start();
-//        String file = "teapot.stl";
-//        InputStream inputStream = null;
-//        ParsedSTL parsedSTL = null;
-//        try {
-//            inputStream = new BufferedInputStream(new FileInputStream(file));
-//            parsedSTL = STLParser.parse(inputStream);
-//
-//        } catch (FileNotFoundException e) {
-//            System.out.println("File not found");
-//        } catch (EOFException e) {
-//        } catch (IOException e) {
-//            System.out.println(Arrays.toString(e.getStackTrace()));
-//        } finally {
-//            try {
-//                if (inputStream != null) {
-//                    inputStream.close();
-//                }
-//            } catch (IOException e) {
-//                System.out.println(Arrays.toString(e.getStackTrace()));
-//            }
-//        }
-//        Mesh cubeBleu = new Box(new Vertex(200, 200, 200), 100, 100, 30, Color.BLUE);
-//        Mesh cubeRouge = new Box(new Vertex(0, 0, 0), 75, 75, 75, Color.RED);
-//        Mesh pyramidVerte = new Pyramid(new Vertex(-110, 110, 110), Color.GREEN);
-//        Mesh ground = new Plane(new Vertex(0, 0, 0), 1000, Color.white);
-//        frame.add(new Renderer(new ArrayList<Mesh>(List.of(cubeRouge, cubeBleu, pyramidVerte, ground))));
     }
 }
