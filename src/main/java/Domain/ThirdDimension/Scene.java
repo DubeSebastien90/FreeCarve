f4b238b8a188f546ccdc48d9a7581616e92e82cb
Kamran Charles Nayebi <kamran-charles.nayebi.1@ulaval.ca>
2024-11-05 11:36:40 -0500

    Change car model for jar export

--- src/main/java/Domain/ThirdDimension/Scene.java
+++ src/main/java/Domain/ThirdDimension/Scene.java
@@ -32,7 +32,7 @@ public class Scene {
     public Scene(){
         // Sets the default scene, the car for now
         try {
-            Mesh car = new Mesh(Vertex.zero(), Color.GRAY, "car.stl", 100);
+            Mesh car = new Mesh(Vertex.zero(), Color.GRAY, getClass().getResource("car.stl").getPath(), 100);
             car.setRotationEuler(new Vertex(0, Math.PI/4, 0));
             setMeshes(List.of(car));
         } catch (Exception e){
