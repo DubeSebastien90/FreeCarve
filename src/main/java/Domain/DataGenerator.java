592b5ea3c117e76d6ef86bdaefd249af0ec0ccef
Adam Côté <adam.cote.1@ulaval.ca>
2024-11-19 18:38:05 -0500

    Gcode tested i think

--- src/main/java/Domain/DataGenerator.java
+++ src/main/java/Domain/DataGenerator.java
@@ -1,6 +1,7 @@
 package Domain;
 
 import Common.DTO.VertexDTO;
+
 import java.util.List;
 
 /**
@@ -42,8 +43,10 @@ class DataGenerator {
                 if (vertex == cut.getPoints().get(0)) {
                     instructions.append("G00 X").append(vertex.getX()).append(" Y").append(vertex.getY()).append(lineEnd); //go to position of first point
                     instructions.append("M03 " + rotationSpeed + lineEnd); //starts the rotation of the tool
+                    instructions.append("G82 X").append(vertex.getX()).append(" Y").append(vertex.getY()).append(" Z").append(vertex.getZ()).append(lineEnd); //drill the first hole
+                } else {
+                    instructions.append("G09 X").append(vertex.getX()).append(" Y").append(vertex.getY()).append(" Z").append(vertex.getZ()).append(lineEnd); //cut to the point location
                 }
-                instructions.append("G09 X").append(vertex.getX()).append(" Y").append(vertex.getY()).append(" Z").append(vertex.getZ()).append(lineEnd); //cut to the point location
             }
             instructions.append("M05" + lineEnd); //stop the tool
             instructions.append("G00 Z0" + lineEnd); //go to the predefined Z safe spot
@@ -51,6 +54,7 @@ class DataGenerator {
         //end program
         instructions.append("M05" + lineEnd); // stops the bit rotation
         instructions.append("G28" + lineEnd); // return to (0,0)
+        instructions.append("G00 X0 Y0" + lineEnd);
         instructions.append("M02" + lineEnd); //end of the program
 
         return instructions.toString();
